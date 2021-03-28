fba869690ec219400e1e99c13ea0bfab7f46b1ed
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-28 15:57:51 -0300

    revert code

--- pages/info.js
+++ pages/info.js
@@ -1,21 +1,15 @@
 import api from "../api";
+import Link from "next/link";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import styles from "../css/Info.module.css";
-import { useEffect, useState } from "react";
 
 export default function Info(data) {
-    const { title, thumbnail, download } = data;
+    const { title, thumbnail } = data;
 
     const router = useRouter();
 
     const { type } = router.query;
-
-    const [downloadUrl, setDownloadUrl] = useState("");
-
-    useEffect(() => {
-        setDownloadUrl(window.URL.createObjectURL(new Blob([download])));
-    }, []);
 
     function handleExit() {
         router.push("/");
@@ -31,13 +25,14 @@ export default function Info(data) {
             </h4>
             <img src={thumbnail} className={styles.cardImage} />
             <div className={styles.submit}>
-                <a
-                    href={downloadUrl}
-                    download={`${title}.${type}`}
-                    className={styles.submitButton}
+                <Link
+                    href={{
+                        pathname: "/download",
+                        query: { type: type, url: url },
+                    }}
                 >
-                    Download
-                </a>
+                    <a className={styles.submitButton}>Download</a>
+                </Link>
                 <a href="/" onClick={handleExit} className={styles.exit}>
                     Voltar
                 </a>
@@ -50,18 +45,13 @@ export default function Info(data) {
 }
 
 export async function getServerSideProps(context) {
-    const { url, type } = context.query;
+    const { url } = context.query;
 
     const response = await api.get(`/info/?url=${url}`);
-
-    const downloadResponse = await api.get(`${type}/?url=${url}`, {
-        responseType: "blob",
-    });
 
     const data = {
         title: response.data.title,
         thumbnail: response.data.thumbnail,
-        download: downloadResponse.data,
     };
 
     return { props: data };
